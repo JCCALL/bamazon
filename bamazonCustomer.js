d7b86a8c50776c593f9073426376978b05abb183
Dark Knight <jordanccall@gmail.com>
2020-05-12 18:18:21 -0600

    added npm for table and got everything looking good through node

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var sqlpass = require("./key");
+var Table = require('cli-table3');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,13 +13,54 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    showItems();
+    
 })
 
 function showItems(){
     var query = connection.query("SELECT * FROM products", function(err, res){
         if(err) throw err;
-        console.log(res);
+        console.log("ID " + " PRODUCT " + " DEPARTMENT " + " PRICE " + " STOCK " + "\n" +
+                    "------------------------------------------------")
+        for (var i = 0; i < res.length; i++){
+        console.log(res[i].item_id + " " + res[i].product_name  + " " + res[i].department_name  + " " + res[i].price  + " " + res[i].stock_quanitity);
+        };
         connection.end();
     })
 };
+
+function createTable(){
+    connection.query('SELECT item_id, product_name, price FROM products', function(err, res){
+        if(err) console.log(err);
+
+    var table = new Table({
+        head: ['Item ID', 'Product', 'Price']
+
+    });
+    for(var i = 0; i < res.length; i++) {
+        table.push(
+            [res[i].item_id, res[i].product_name, res[i].price]
+        );
+    }
+    });
+    console.log(table.toString());
+    connection.end();
+};
+
+
+connection.query('SELECT item_id, product_name, price FROM products', function(err, res){
+    if(err) console.log(err); 
+//instantiate
+var table = new Table({
+    head: ['Item ID', 'Product', 'Price']
+});
+ 
+// table is an Array, so you can `push`, `unshift`, `splice` and friends
+for(var i = 0; i < res.length; i++) {
+    table.push(
+        [res[i].item_id, res[i].product_name, res[i].price]
+    );
+    }
+
+console.log(table.toString());
+connection.end();
+});

acccad7d9329ab4abd57e732d0a42814c3a7b997
Dark Knight <jordanccall@gmail.com>
2020-05-12 22:25:30 -0600

    got the customer side up and running

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,56 +11,72 @@ var connection = mysql.createConnection({
 	database: "bamazon"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    
-})
-
-function showItems(){
-    var query = connection.query("SELECT * FROM products", function(err, res){
-        if(err) throw err;
-        console.log("ID " + " PRODUCT " + " DEPARTMENT " + " PRICE " + " STOCK " + "\n" +
-                    "------------------------------------------------")
-        for (var i = 0; i < res.length; i++){
-        console.log(res[i].item_id + " " + res[i].product_name  + " " + res[i].department_name  + " " + res[i].price  + " " + res[i].stock_quanitity);
-        };
-        connection.end();
-    })
-};
-
-function createTable(){
-    connection.query('SELECT item_id, product_name, price FROM products', function(err, res){
-        if(err) console.log(err);
-
-    var table = new Table({
-        head: ['Item ID', 'Product', 'Price']
-
-    });
-    for(var i = 0; i < res.length; i++) {
-        table.push(
-            [res[i].item_id, res[i].product_name, res[i].price]
-        );
-    }
-    });
-    console.log(table.toString());
-    connection.end();
-};
+connection.connect();
 
 
 connection.query('SELECT item_id, product_name, price FROM products', function(err, res){
     if(err) console.log(err); 
-//instantiate
+
 var table = new Table({
     head: ['Item ID', 'Product', 'Price']
 });
- 
-// table is an Array, so you can `push`, `unshift`, `splice` and friends
+
 for(var i = 0; i < res.length; i++) {
     table.push(
         [res[i].item_id, res[i].product_name, res[i].price]
     );
     }
 
-console.log(table.toString());
-connection.end();
+console.log(table.toString()+ "\n");
+purchase();
 });
+
+function purchase(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the Item ID of the item you would like to purchase.",
+            name: "itemID",
+            filter: Number
+        },
+        {
+            type: "input",
+            message: "How many would you like to purchase?",
+            name: "quantity",
+            filter: Number
+        },
+        {
+            type: "confirm",
+            message: "Are you sure?",
+            name: "confirm",
+            default: true
+        }
+
+    ])
+    .then(function(inquirerRes) {
+        if (inquirerRes.confirm){
+            var quantReq = inquirerRes.quantity;
+            var idReq = inquirerRes.itemID;
+            purchaseOrder(idReq, quantReq);
+        } else{
+            console.log("That is okay please come again")
+            connection.end();
+        }    
+    });
+};
+function purchaseOrder(ID, amt){
+    connection.query("Select * FROM products WHERE item_id = " + ID, function(err, res){
+        if (err) { console.log(err);};
+        if ( amt <= res[0].stock_quantity) {
+            var cost = res[0].price * amt;
+            console.log("Amount requested is available!");
+            console.log("Your total cost for " + amt + " " + res[0].product_name + " is " + "$" + cost + " Thank you for your business.");
+
+            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amt + " WHERE item_id = " + ID);
+        } else {
+            console.log("I am sorry to inform you that we do not have enough " + res[0].product_name + " to complete your order.");
+        };
+        connection.end();
+    });
+    
+}
